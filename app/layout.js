cff16328af24eadcdf658d1bec6b9df7982cae94
sankalp56 <sankalpask56@gmail.com>
2025-05-11 19:09:13 +0530

    Updated layout.js

--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,7 @@ import { ThemeProvider } from "../components/theme-provider";
 import { dark } from "@clerk/themes";
 import { Toaster } from "sonner";
 
-const inter = Inter({subsets:["Latin"]});
+const inter = Inter({subsets:["latin"]});
 
 export const metadata = {
   title: "AI Career Assistant",
